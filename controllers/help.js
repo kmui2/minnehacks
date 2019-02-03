ba46d950dcc6230ade80d79cc3c8ce65a45ad3ff
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2019-02-03 11:48:07 -0600

    fix spaces

--- controllers/help.js
+++ controllers/help.js
@@ -2,13 +2,13 @@ const routes = require("../routes/routes");
 
 module.exports = (req) => {
   let result = `
-  Valid options:
-  1. Weather (city) - get the forecast for a city
-  2. News [query] - get the news about a topic
-  3. About (subject) - learn about a subject
-  4. Fact - get a useful fact about farming
-  5. Help - print this help text
-  `;
+Valid options:
+1. Weather (city) - get the forecast for a city
+2. News [query] - get the news about a topic
+3. About (subject) - learn about a subject
+4. Fact - get a useful fact about farming
+5. Help - print this help text
+`;
   
   return [result];
 }
