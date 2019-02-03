77d3a40ca220a45c1ef3090d0bedafc152cbe8ee
Isaac Lo <isaaclo123@gmail.com>
2019-02-03 02:57:07 -0600

    weather may work

--- helpers/weatherHelpers.js
+++ helpers/weatherHelpers.js
@@ -1,11 +1,11 @@
-function getWeatherString (data){
+module.exports.getWeatherString = (data) => {
   let str = "";
-  str += data.date + "\n";
-  str += data.temp_l + " ºC\n";
-  str += data.temp_h + " ºC\n";
-  str += data.wind_speed + " km/h\n";
-  str += data.description + "\n";
-  str += data.humidity + " %";
+
+  str += data.date.toString() + "\n";
+  str += data.temp_l.toString() + " - " + data.temp_h.toString() + " ºC\n";
+  str += data.wind_speed.toString() + " km/h\n";
+  str += data.description.toString() + "\n";
+  str += data.humidity.toString() + " %";
 
   return str;
 }
