b93ad626530bcd7be7e7df89f03583b8dba0604a
Marcus <ohxxx371@umn.edu>
2019-02-03 01:07:17 -0600

    weather formatting

---
+++ helpers/weatherHelpers.js
@@ -0,0 +1,11 @@
+function getWeatherString (data){
+  let str = "";
+  str += data.date + "\n";
+  str += data.temp_l + " ºC\n";
+  str += data.temp_h + " ºC\n";
+  str += data.wind_speed + " km/h\n";
+  str += data.precipitation + " cm\n";
+  str += data.humidity + " %";
+
+  return str;
+}
